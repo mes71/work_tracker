669dc7af48cb1109eee2e205516b5105004922ea
mojtaba <mr.mojtaba47@gmail.com>
2024-10-14 12:46:32 +0330

    feat :  auth with firebase

---
+++ lib/data/model/user/user_model.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      id: json['id'] as String?,
+      email: json['email'] as String?,
+      refToken: json['refToken'] as String?,
+      name: json['name'] as String?,
+      photoUrl: json['photoUrl'] as String?,
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'refToken': instance.refToken,
+      'name': instance.name,
+      'photoUrl': instance.photoUrl,
+    };
